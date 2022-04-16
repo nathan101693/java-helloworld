fa0f1c1ffc91f48ed3803eb0a43708d528d6e8d8
natha <natha@10.0.0.113>
2022-04-16 10:38:14 -0500

    First commit

---
+++ tech.strategio.helloworld/src/tech/strategio/helloworld2/HelloWorldClass.java
@@ -0,0 +1,11 @@
+package tech.strategio.helloworld2;
+
+public class HelloWorldClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World");
+	}
+
+}
